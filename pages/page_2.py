7d98c5ac190a403e89691f167c5cd004f6120093
Caio Polari <157854150+caiopolari@users.noreply.github.com>
2024-07-10 18:05:48 -0300

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -6,7 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 from datetime import datetime
-from haversine import haversine
 from PIL import Image
 import folium
 from streamlit_folium import folium_static
